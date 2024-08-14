ba8f6a64c99fdb791a846190c9881f4741d5d651
Gavin Fu <gavin_fu@hotmail.com>
2024-08-14 23:34:43 +0800

    fix Listener Bug

--- lib/external_display.dart
+++ lib/external_display.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-// Provides the 'ExternalDisplay' method.
+/// Provides the 'ExternalDisplay' method.
 class ExternalDisplay {
   final List<Function(dynamic)> _listeners = [];
-  StreamSubscription? _streamSubscription;
   bool _isPlugging = false;
-
-  // external monitor resolution
   Size? _currentResolution;
 
   static const MethodChannel _displayController =
@@ -15,7 +12,24 @@ class ExternalDisplay {
   static const EventChannel _monitorStateListener =
       EventChannel('monitorStateListener');
 
-  // Connect an external monitor and get the resolution
+  /// Initialize ExternalDisplay class
+  ExternalDisplay() {
+    StreamSubscription streamSubscription =
+        _monitorStateListener.receiveBroadcastStream().listen((event) {
+      _isPlugging = event;
+      for (var listener in _listeners) {
+        listener(event);
+      }
+    });
+
+    _finalizer.attach(this, streamSubscription);
+  }
+
+  static final _finalizer = Finalizer<StreamSubscription>((streamSubscription) {
+    streamSubscription.cancel();
+  });
+
+  /// Connect an external monitor and get the resolution
   Future connect({String? routeName}) async {
     final size = await _displayController
         .invokeMethod('connect', {"routeName": routeName});
@@ -24,43 +38,31 @@ class ExternalDisplay {
     }
   }
 
-  // get resolution
+  /// get resolution
   Size? get resolution {
     return _currentResolution;
   }
 
-  // plugging status
+  /// plugging status
   bool get isPlugging {
     return _isPlugging;
   }
 
-  // Send parameters to external display page
+  /// Send parameters to external display page
   Future<bool> transferParameters(
       {required String action, required dynamic value}) async {
     return await _displayController
         .invokeMethod('transferParameters', {"action": action, "value": value});
   }
 
-  // Monitor external monitor plugging and unplugging
+  /// Monitor external monitor plugging and unplugging
   void addListener(Function(dynamic) listener) {
-    if (_listeners.isEmpty) {
-      _streamSubscription =
-          _monitorStateListener.receiveBroadcastStream().listen((event) {
-        _isPlugging = event;
-        for (var listener in _listeners) {
-          listener(event);
-        }
-      });
-    }
     _listeners.add(listener);
   }
 
-  // Cancel monitoring of external monitor
+  /// Cancel monitoring of external monitor
   bool removeListener(Function listener) {
     final result = _listeners.remove(listener);
-    if (_listeners.isEmpty) {
-      _streamSubscription?.cancel();
-    }
 
     return result;
   }
--- lib/receive_parameters.dart
+++ lib/receive_parameters.dart
@@ -1,32 +1,37 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-// Provides the 'ReceiveParameters' method.
+/// Provides the 'ReceiveParameters' method.
 class ReceiveParameters {
   final List<Function({required String action, dynamic value})> _listeners = [];
-  StreamSubscription? _streamSubscription;
   static const EventChannel _receiveParametersListener =
       EventChannel('receiveParametersListener');
 
-  // Monitor receiving parameters
+  /// Initialize ReceiveParameters class
+  ReceiveParameters() {
+    /// add parameters listener
+    StreamSubscription streamSubscription =
+        _receiveParametersListener.receiveBroadcastStream().listen((event) {
+      for (var listener in _listeners) {
+        listener(action: event["action"], value: event["value"]);
+      }
+    });
+
+    _finalizer.attach(this, streamSubscription);
+  }
+
+  static final _finalizer = Finalizer<StreamSubscription>((streamSubscription) {
+    streamSubscription.cancel();
+  });
+
+  /// Monitor receiving parameters
   void addListener(Function({required String action, dynamic value}) listener) {
-    if (_listeners.isEmpty) {
-      _streamSubscription =
-          _receiveParametersListener.receiveBroadcastStream().listen((event) {
-        for (var listener in _listeners) {
-          listener(action: event["action"], value: event["value"]);
-        }
-      });
-    }
     _listeners.add(listener);
   }
 
-  // Cancel receiving parameters
+  /// Cancel receiving parameters
   bool removeListener(Function listener) {
     final result = _listeners.remove(listener);
-    if (_listeners.isEmpty) {
-      _streamSubscription?.cancel();
-    }
 
     return result;
   }
